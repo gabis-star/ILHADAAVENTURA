353917893200ec1d2df5863dce2124bd4a16cf53
gabis-star <169148839+gabis-star@users.noreply.github.com>
2024-09-17 07:39:42 -0300

    ht

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
-avanca.forEach(BUTTON => {
-    BUTTON.addEventListener('click' , function(){
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
-        const proximoPasso = 'passo-' + this.geatAttribute('data-proximo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
         atual.classList.remove('ativo');
-document.getElementById(proximoPasso).classList.add('ativo');
-    
+        document.getElementById(proximoPasso).classList.add('ativo');
     })
 })
